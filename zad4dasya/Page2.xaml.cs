5f56b4fd320dce82ea7491338dd3a1a9f1066e8a
dasyona <dasyaski@yandex.ru>
2026-06-11 14:02:26 +0500

    Add files via upload

---
+++ zad4dasya/Page2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace zad4dasya
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            myDatePicker.Date = DateTime.Today; // Ставим текущую дату при старте
+            LoadRates();
+        }
+        // Вызывается автоматически при выборе даты в DatePicker
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            LoadRates();
+        }
+
+        private void LoadRates()
+        {
+            USD.Text = "USD: 80 руб.";
+            EUR.Text = "EUR: 86 руб.";
+        }
+    }
+}
